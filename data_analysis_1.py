503734613807c075e5f4e1c5177becf72b0d9ee9
S-Chaudhuri <103317678+S-Chaudhuri@users.noreply.github.com>
2022-06-19 15:52:14 +0530

    Update data_analysis_1.py
    first python code

--- data_analysis_1.py
+++ data_analysis_1.py
@@ -1,7 +1,7 @@
-print("Enter a integer for a day in the week(0-6)!")
+print("Enter a integer for a day in the week(0-6)!")#code to find day in week
 a = int(input())
 print("Enter number of days after today.")
 b = int(input())
 d=b%7
 week=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-print(f'The day is { week[(a+d-1)%7]}')
+print(f'The day is { week[(a+d-1)%7]}') #reducing the numbers to be useable
